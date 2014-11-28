cbb88a4a6f869fb358c5177bccc1872b09894e11
gonzalowtf77 <gonzalowtf@gmail.com>
2014-11-28 21:28:15 +0000

    starting chat , trying to

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var express = require("express"),
     ip=process.env.IP;
     
     
+    
 app.configure(function () {
   app.use(express.bodyParser()); 
   app.use(express.methodOverride());
@@ -24,7 +25,15 @@ console.log(ip+":"+port);
 
 
 app.get('/', function(req, res) {
-  res.sendfile(__dirname + '/index.html');
+      console.log(req.url);
+      if(req.url === '/'){
+          res.sendfile(__dirname + '/index.html');      
+      }
+      if(req.url === '/emergente/emergente/chat.html') {
+        res.sendfile(__dirname +'/emergente/emergente/chat.html');
+      }
+
+  
 });
 
 
@@ -46,6 +55,41 @@ mongoose.connect("mongodb://gonzalowtf:aereomodelismo12@ds035750.mongolab.com:35
   }
 });
 
+//--------------------------------------------------------------------------------------- socket
+//for get data into server
+
+// when the connection is ready everywhere here appears this
+io.sockets.on("connection",function(socket){
+
+// for the online users ta the moment od connection
+	socket.on("new-user",function(data, callback){
+
+       if(nicknames.indexOf(data) != -1){
+       	callback(false);
+       }
+       else{
+       	callback(true);
+       	socket.nickname=data;
+       	nicknames.push(socket.nickname);
+       	updatenicknames();
+       }
+
+	});
+    //"send-mesage" and "new message" are the functions on index and works everywhere
+    socket.on("send-message" , function(data){
+    	io.sockets.emit('new-message',{msg:data , nick : socket.nickname});
+    });
+
+function updatenicknames(){
+	       	io.sockets.emit("usernames",nicknames);
 
 
+}
 
+  socket.on("disconnect" , function(data){
+
+  			if(!socket.nickname) return;
+  			nicknames.splice(nicknames.indexOf(socket.nickname),1);
+  			updatenicknames();
+  });
+});
